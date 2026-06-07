587f43173d525abedbf0f342624e5775f3ff9aa6
viktorashi <ioanvictorstan@gmail.com>
2026-06-08 01:14:20 +0300

    viktora.sh ca hostname

--- nsa/build.sh
+++ nsa/build.sh
@@ -1,1 +1,1 @@
-cd web-server/ && CGO_ENABLED=0 GOOS=linux go build -ldflags="-s -w" -o out/web-server .
+clear && cd web-server/ && CGO_ENABLED=0 GOOS=linux go build -ldflags="-s -w" -o out/web-server .
